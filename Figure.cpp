90161ddd9abf8ea0c79ca970e29f81a25f71558a
Konstantin2702 <klaptev15337@gmail.com>
2020-03-30 20:31:33 +0300

    Some problems,30.03.2020

--- Figure.cpp
+++ Figure.cpp
@@ -1,16 +1,20 @@
-#include "Figure.h"
 #include <iostream>
+#include "Figure.h"
 
 //--constructor--
 
 Figure::Figure()
 {
-	activePoint = Point();
+	/*Point* activePoint = new Point[1];
+	*activePoint = Point();*/
 }
 
-Figure::Figure(int _x, int _y, int _name)
+Figure::Figure(Point* obj)
 {
-	activePoint = Point(_x, _y, _name);
+	Point* activePoint = new Point[1];
+	activePoint = obj;
+	count = 1;
+	activePoint->name = 1;
 }
 
 //destructor
@@ -23,12 +27,12 @@ Figure::~Figure()
 //--methods--
 
 //добавление точки (проведение линии от актитвной точки к новой)
-void Figure::addPoint(Point obj)
+void Figure::addPoint(Point& obj)
 {
 	count++;
 	obj.name = count;
-	activePoint.contPoint(obj);
-	obj.contPoint(activePoint);
+	activePoint->contPoint( obj);
+	obj.contPoint(*activePoint);
 }
 //удаление линии
 void Figure::deleteLine(Point& obj1, Point& obj2)
@@ -39,33 +43,66 @@ void Figure::deleteLine(Point& obj1, Point& obj2)
 //соединение двух точек
 void Figure::contIsolPoint(Point& obj1, Point& obj2)
 {
-	obj1.contPoint(obj2);
-	obj2.contPoint(obj1);
+	obj1.contPoint(obj1);
+	obj2.contPoint(obj2);
 }
 //создание фигуры
-void Figure::createFigure(Point _point)
+void Figure::createFigure(Point* _point)
 {
 	activePoint = _point;
 	count = 1;
-	activePoint.name = count;
+	activePoint->name = count;
 }
 //текущий статус
 void Figure::status()
 {
 	std::cout << "--status--" << std::endl;
-	std::cout << "Active point: #" << activePoint.getName() << std::endl;
+	std::cout << "Active point: #" << activePoint->getName() << std::endl;
 	std::cout << "Amount of points: " << count << std::endl;
 	std::cout << "----------" << std::endl;
 }
 
 
+
+//bypass for search the point
+Point* Figure::byPass(int _name, Point& _obj) //  in the first obj is active point
+{
+	bool found = false;
+	for (int i = 0; i < _obj.getNumbSon(); i++)
+	{
+		if (_obj.getArrPoints()[i].isLocked() == true)
+			continue;
+		else if (_obj.getArrPoints()[i].getName() != _name)
+			continue;
+		else
+		{
+			std::cout << _obj.getArrPoints()[i].getName();
+			found = true;
+			return  &_obj.getArrPoints()[i];
+		}
+	}
+	_obj.lock();
+	if (found != true)
+	{
+		for (int i = 0; i < _obj.getNumbSon(); i++)
+		{
+			if (_obj.getArrPoints()[i].isLocked() == true)
+				continue;
+			else
+				byPass(_name, _obj.getArrPoints()[i]);
+		}
+	}
+}
+
+
+
 //--properties--
 
-Point& Figure::getActivePoint()
+Point* Figure::getActivePoint()
 {
 	return activePoint;
 }
-void Figure::setActPoint(Point& obj)
+void Figure::setActPoint(Point* obj)
 {
 	activePoint = obj;
 }
--- Figure.h
+++ Figure.h
@@ -1,35 +1,37 @@
 #pragma once
 #include "Point.h"
 class Figure
-{   
+{
 private:
 
 	//the point we are working with
-	Point activePoint;
+	Point* activePoint;
 	//amount of points
 	int count;
 
 public:
 	//constructor
 	Figure();
-	Figure(int _x, int _y, int _name);
+	Figure(Point* obj);
 	//destructor
 	~Figure();
 
 
 	//--methods--
 
-	void addPoint(Point obj1);
+	void addPoint(Point& obj1);
 	void deleteLine(Point& obj1, Point& obj2);
-	void createFigure(Point _point);
+	void createFigure(Point* _point);
 	void status();
 	void contIsolPoint(Point& obj1, Point& obj2);
+	Point* byPass(int _name, Point& _obj);
 
 
 	//--properties--
 
-	Point& getActivePoint();
-	void setActPoint(Point& obj);
+	Point* getActivePoint();
+	void setActPoint(Point* obj);
 	int getCount();
+
 
 };
